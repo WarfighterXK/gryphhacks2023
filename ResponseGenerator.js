e3e98393f0195a0a0efe6288f2436b8549e0e373
WarfighterXK <74974138+WarfighterXK@users.noreply.github.com>
2023-05-27 17:28:37 -0400

    Add files via upload
    bug

--- ResponseGenerator.js
+++ ResponseGenerator.js
@@ -1,12 +1,45 @@
-const { Configuration, OpenAIApi } = require("openai")
+// const { Configuration, OpenAIApi } = require("openai")
 
+// const configuration = new Configuration({
+//   apiKey: "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"
+// })
+// const openai = new OpenAIApi(configuration)
+
+// const completion = openai.createCompletion({
+//   model: "text-davinci-003",
+//   prompt: "Hello world"
+// })
+// console.log(completion.data.choices[0].text)
+
+// const response = "Default response"
+console.log('test response generator run')
+
+const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
-  apiKey: "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"
-})
-const openai = new OpenAIApi(configuration)
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
-const completion = openai.createCompletion({
-  model: "text-davinci-003",
-  prompt: "Hello world"
-})
-console.log(completion.data.choices[0].text)
+async function getResponse() {
+  return await openai.createCompletion({
+    model: "gpt-3.5-turbo",
+    prompt: "Say this is a test and tell me who Scottie Barnes is.",
+    temperature: 0,
+    max_tokens: 7,
+  });
+}
+
+const response = getResponse()
+// const response = await openai.createCompletion({
+//   model: "gpt-3.5-turbo",
+//   prompt: "Say this is a test and tell me who Scottie Barnes is.",
+//   temperature: 0,
+//   max_tokens: 7,
+// });
+
+var defResponse = "Default response"
+// export {defResponse}
+
+// localStorage.setItem("defResponse", defResponse)
+
+console.log(response.data.choices[0].text)
--- background.js
+++ background.js
@@ -1,19 +1,62 @@
 // TODO: needs to be changed - raises an error because I have not specified a tab.
-var port = chrome.tabs.connect({ name: "portConnection" })
+// var port = chrome.tabs.connect({ name: "portConnection" })
 
-chrome.tabs.query({}, tabs => {
-  tabs.forEach(() => {
-    console.log("Getting content...")
-    port.postMessage({ action: "getContent" })
-  })
-})
+// chrome.tabs.query({}, tabs => {
+//   tabs.forEach(() => {
+//     console.log("Getting content...")
+//     port.postMessage({ action: "getContent" })
+//   })
+// })
 
-// Setting up listener.
-port.onMessage.addListener(function (request) {
-  console.log("Listening service-worker...")
+console.log('test response generator run')
 
-  switch (request.action) {
-    case "logContent":
-      console.log(request.content)
-  }
-})
+// try {
+//   importScripts('/node_modules/openai/dist/api.js', '/node_modules/openai/dist/configuration.js', '/node_modules/openai/dist/index.js'/*'ResponseGenerator.js' *//*, and so on */);
+// } catch (e) {
+//   console.log("potato")
+//   console.log(e)
+//   // console.error(e);
+// }
+
+import {Configuration, OpenAIApi} from './node_modules/openai/index.ts'
+
+// import { Configuration, OpenAIApi } from '/node_modules/openai/index.ts';
+// import './node_modules/openai/index.ts';
+// import './node_modules/openai'
+// import { Configuration } from "../node_modules/openai/configuration.ts";
+// import { OpenAIApi, Configuration } from "./node_modules/openai/index.ts";
+
+// import { Configuration, OpenAIApi } from "openai";
+
+
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+const response = openai.createCompletion({
+  model: "gpt-3.5-turbo",
+  prompt: "Say this is a test.",
+  temperature: 0,
+  max_tokens: 7,
+});
+
+var defResponse = "Default response"
+// export {defResponse}
+console.log('testyy')
+
+localStorage.setItem("defResponse", defResponse)
+
+console.log(response.data.choices[0].text)
+
+
+
+// // Setting up listener.
+// port.onMessage.addListener(function (request) {
+//   console.log("Listening service-worker...")
+
+//   switch (request.action) {
+//     case "logContent":
+//       console.log(request.content)
+//   }
+// })
