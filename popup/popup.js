f9d3df730f9b8f82d6ea67bc0a483294c1751081
WarfighterXK <74974138+WarfighterXK@users.noreply.github.com>
2023-05-27 17:29:29 -0400

    Add files via upload

--- popup/popup.js
+++ popup/popup.js
@@ -6,6 +6,8 @@ currentTaskInput.addEventListener("change", handleInput)
 const submitButton = document.getElementById("task-submit-button")
 submitButton.addEventListener("click", handleClicked)
 
+const body = document.getElementById("body")
+
 let currentValue
 
 function handleInput() {
@@ -15,5 +17,13 @@ function handleInput() {
 
 function handleClicked() {
   submitButton.innerHTML = "hi"
-  console.log("clicked the button")
+  console.log("Clicked the button")
+
+  // const defResponse = window.parent.defResponse
+  console.log("Before getting defResponse.")
+  const testDefRepsonse = localStorage.getItem("defResponse")
+  console.log(testDefRepsonse)
+  // console.log(defResponse)
+  body.innerHTML = testDefRepsonse
+  console.log("Response generated.")
 }
